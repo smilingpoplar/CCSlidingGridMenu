ffc8db37349be0150ea35337b4db9a357a7c766f
smilingpoplar <smilingpoplar@gmail.com>
2012-12-16 21:07:32 +0800

    Initial Commit

---
+++ CCSlidingGridMenu/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  CCSlidingGridMenuAppController.h
+//  CCSlidingGridMenu
+//
+//  Created by smilingpoplar on 12-12-16.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ CCSlidingGridMenu/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  CCSlidingGridMenuAppController.h
+//  CCSlidingGridMenu
+//
+//  Created by smilingpoplar on 12-12-16.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
